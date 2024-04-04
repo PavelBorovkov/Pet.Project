7bb190a2bdd4550697fce86e7650005e9355d8a1
Pavel Borovkov(PC) <pavel3458@mail.ru>
2024-04-04 16:48:57 +0500

    Добавьте файлы проекта.

---
+++ Product.API/Product.Application/Products/Queries/GetProductList/GetProductListQueryHandler.cs
@@ -0,0 +1,38 @@
+﻿using AutoMapper;
+using AutoMapper.QueryableExtensions;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestTask.Application.Interfaces;
+using TestTask.Application.Common.Exceptions;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestTask.Application.Products.Queries.GetProductList
+{
+    public class GetNoteListQueryHandler
+        :IRequestHandler<GetProductListQuery,ProductListVm>
+    {
+        private readonly ITestTaskDbContext _dbContext;
+        private readonly IMapper _mapper;
+
+        public GetNoteListQueryHandler(ITestTaskDbContext dbContext, IMapper mapper)
+        {
+            _dbContext = dbContext;
+            _mapper = mapper;
+        }
+        public async Task<ProductListVm>Handle(GetProductListQuery request,
+            CancellationToken cancellationToken)
+        {
+            var productQuery = _dbContext.Products.ProjectTo<ProductLookupDto>(_mapper.ConfigurationProvider);
+            if (!string.IsNullOrEmpty(request.Name))
+            {
+                productQuery = productQuery.Where(p => p.Name.Contains(request.Name));
+            }
+
+            return new ProductListVm { Products = productQuery.ToList() };
+        }
+    }
+}
